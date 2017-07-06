ff0d7a10b327bd3b6e014750a339a8bf7978a9ac
Tom <scarlet0345@gmail.com>
2017-07-06 22:15:22 +0800

    boilerplate testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './app.less';
 
 
 class App extends React.Component {
     render () {
         return (
-            <h1>Hello</h1>
+            <h1>TwT BBS Testing Page</h1>
         );
     }
 }
