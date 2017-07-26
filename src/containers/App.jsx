3c448306d8f8fcca492f7005108f1e5bccf4f36d
Tom <scarlet0345@gmail.com>
2017-07-26 17:48:37 +0800

    configured router

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ForumList from './ForumList';
+import { Switch, Route } from 'react-router-dom';
+import Forums from './ForumList';
+import Home from './Home';
+import Me from './Me';
+import NoMatch from '../components/NoMatch';
 
 
-class App extends React.Component {
-    render () {
-        return (
-            <ForumList />
-        );
-    }
-}
+const App = () => (
+    <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/forums' component={Forums} />
+        <Route path='/user/me' component={Me} />
+        <Route component={NoMatch} />
+    </Switch>
+);
 
 export default App;
