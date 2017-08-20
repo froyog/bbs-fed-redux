1bf20252b471181c64ef7f4474667bbabd1c3cbf
Tom <scarlet0345@gmail.com>
2017-08-20 17:05:28 +0800

    applyMiddlewares bug fixed

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/rootReducer';
-import { middlewares } from './middlewares';
+import thunk from 'redux-thunk';
+import RavenMiddleware from 'redux-raven-middleware';
+import callApi from '../middlewares/callApi';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,7 +12,7 @@ const configureStore = preloadedState => {
         rootReducer,
         preloadedState,
         composeEnhancers(
-            applyMiddleware(...middlewares)
+            applyMiddleware(thunk, callApi)
         )
     );
 
