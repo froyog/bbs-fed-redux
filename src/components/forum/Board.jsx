edfd30f909cedcacffefa9c682e2730864c94912
Tom <scarlet0345@gmail.com>
2017-08-22 10:35:07 +0800

    add & demo for Time component

--- src/components/forum/Board.jsx
+++ src/components/forum/Board.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FetchingOverlay from '../common/FetchingOverlay';
 import { Card } from '../common/Card';
+import Time from '../common/Time';
 
 
 const Board = ({ boardInfo, threads }) => {
     const { name } = boardInfo;
+    const { tReply } = threads[0];
+
     return (
         <Card title={name}>
+            <Time timestamp={tReply} />
         </Card>
     );
 };
