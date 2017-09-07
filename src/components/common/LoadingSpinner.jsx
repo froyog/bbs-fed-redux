41056f450fdb7e07c91fe7bd2d678dd7f94a3b4c
Tom <scarlet0345@gmail.com>
2017-09-07 11:18:02 +0800

    add loading spinner

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../styles/common/loading.less';
+
+const LoadingSpinner = () =>
+    <div className="cp-spinner cp-hue"></div>
+
+export default LoadingSpinner;
