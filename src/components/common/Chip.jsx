f99038f1aca15c9da8d8c13ea19cd1917c006ad1
Tom <scarlet0345@gmail.com>
2017-11-24 17:07:50 +0800

    add chips

---
+++ src/components/common/Chip.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../styles/common/chip.less';
+
+
+class Chip extends React.Component {
+    static propTypes = {
+        label: PropTypes.string.isRequired,
+        avatar: PropTypes.element
+    }
+
+    constructor () {
+        super();
+        this.state = {
+            chipIsExpand: false
+        };
+
+        this.handleExpandChip = this.handleExpandChip.bind(this);
+    }
+
+    handleExpandChip () {
+        this.setState({
+            chipIsExpand: true
+        });
+    }
+
+    render () {
+        const { chipIsExpand } = this.state;
+        const { avatar, label } = this.props;
+
+        return (
+            <div className={`chip${chipIsExpand ? ' active' : ''}`} onClick={this.handleExpandChip}>
+                <div className="pull-left avatar">
+                    { avatar && avatar }
+                </div>
+                <span className="label">
+                    { label }
+                </span>
+                {
+                    chipIsExpand &&
+                    <span className="icon-group pull-right">
+                    </span>
+                }
+            </div>
+        )
+    }
+}
+
+export default Chip;
