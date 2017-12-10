547e8bf32d565df0716a217c1ce1d8b7978e1940
Tom <scarlet0345@gmail.com>
2017-12-10 20:01:23 +0800

    Profile: refactor action & reducer

--- src/actions/profile/profile.js
+++ src/actions/profile/profile.js
@@ -4,15 +4,17 @@ import { parseUser } from '../../util';
 export const GET_PROFILE_REQUEST = 'GET_PROFILE_REQUEST';
 export const GET_PROFILE_SUCCESS = 'GET_PROFILE_SUCCESS';
 export const GET_PROFILE_FAILURE = 'GET_PROFILE_FAILURE';
+export const GET_MYSELF_PROFILE_REQUEST = 'GET_MYSELF_PROFILE_REQUEST';
+export const GET_MYSELF_PROFILE_SUCCESS = 'GET_MYSELF_PROFILE_SUCCESS';
+export const GET_MYSELF_PROFILE_FAILURE = 'GET_MYSELF_PROFILE_FAILURE';
 
-const fetchProfile = (uid, authentication) => {
-    let profileAction;
-    // add authentication for self profile request
-    if (uid === 'me' && authentication) {
-        const selfUid = authentication.substring(0, authentication.indexOf('|'));
-        profileAction = {
+export const fetchProfile = (uid, authentication, isMyself) => {
+    if (isMyself) {
+        return {
             [CALL_API]: {
-                types: [GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, GET_PROFILE_FAILURE],
+                types: [GET_MYSELF_PROFILE_REQUEST, 
+                        GET_MYSELF_PROFILE_SUCCESS, 
+                        GET_MYSELF_PROFILE_FAILURE],
                 apiPath: 'home',
                 request: {
                     method: 'GET',
@@ -21,25 +23,29 @@ const fetchProfile = (uid, authentication) => {
                     }
                 }
             },
-            uid: +selfUid,
-        };
-    } else {
-        profileAction = {
-            [CALL_API]: {
-                types: [GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, GET_PROFILE_FAILURE],
-                apiPath: `user/${uid}/home`
-            },
-            uid: +uid
+            uid: +uid,
         };
     }
-    return profileAction;
+    return {
+        [CALL_API]: {
+            types: [GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, GET_PROFILE_FAILURE],
+            apiPath: `user/${uid}/home`
+        },
+        uid: +uid
+    };
 };
 
-export const getProfileIfNeeded = uid => (dispatch, getState) => {
+export const getProfileIfNeeded = locationUid => (dispatch, getState) => {
+    const uid = locationUid === 'me'
+        ? getState().getIn(['user', 'uid'])
+        : locationUid;
+
     const profileByUid = getState().getIn(['profile', uid]);
+    console.log(profileByUid);
     if (profileByUid) {
         return null;
     }
+
     const authentication = parseUser(getState());
-    return dispatch(fetchProfile(uid, authentication));
+    return dispatch(fetchProfile(uid, authentication, locationUid === 'me'));
 };
