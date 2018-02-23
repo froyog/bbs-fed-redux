9d54b25b39ad3e7c5a0fe43a7ffab3f619e7febc
Tom <scarlet0345@gmail.com>
2018-02-23 21:33:34 +0800

    version 2.0.2 ready

--- src/containers/frame/Header.jsx
+++ src/containers/frame/Header.jsx
@@ -20,7 +20,7 @@ class HeaderWrapper extends React.PureComponent {
             postingModalOpen: false,
             headerContent: '',
             tapIsShow: false,
-            updateDate: Date.parse('2018-1-30')
+            updateDate: Date.parse('2018-2-23')
         };
 
         this.handleOpenModal = this.handleOpenModal.bind(this);
